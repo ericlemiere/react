ade2146533d32658a0c09e5b157457a3909e3496
ericlemiere <83970546+ericlemiere@users.noreply.github.com>
2022-09-29 02:13:52 -0700

    bg updated

--- el-portfolio/frontend_react/src/container/Header/Header.jsx
+++ el-portfolio/frontend_react/src/container/Header/Header.jsx
@@ -18,7 +18,7 @@ const scaleVariants = {
 
 const Header = () => {
   return (
-    <div className="app__header app__flex">
+    <div className="app__header app__flex" id="home-bg">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
